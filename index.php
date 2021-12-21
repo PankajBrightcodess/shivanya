c2555e6ec20a742696b56b4c0356ef9d83179486
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-12-21 18:21:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Sankalp Academy</title>
+    <title>Shivanya</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<?php include"head-links.php" ; ?>
     <!-- Start Slider HEAD section -->
