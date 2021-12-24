1e25db7668c31b74114639de6d1c0148c8d965bc
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-12-24 15:15:53 +0530

    Update connection.php

--- admin/connection.php
+++ admin/connection.php
@@ -3,7 +3,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$conn=new mysqli("localhost","root","","db_shivanya");
 }
 else{
-	$conn=new MySQLi("localhost","softwarebss_shivanya","shivanya@2021","softwarebss_shivanya");
+	$conn=new MySQLi("localhost","softwarebss_shivanya","Shivanya@123#","softwarebss_shivanya");
 }
 if($conn->connect_errno){
 	echo "connection failed";
