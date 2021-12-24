ba0ed82931288044cd8b105a8ef0949e95ae91b7
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-12-24 16:08:24 +0530

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -6,7 +6,7 @@ error_reporting(0);
 ?>
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FFF;">
 	<div class="container">
-		<a class="navbar-brand" href="index.php"><img src="../images/logo/logo.jpeg" height="50" width="70" alt="logo"></a>
+		<a class="navbar-brand" href="index.php"><img src="/../images/logo/logo.jpeg" height="50" width="70" alt="logo"></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
