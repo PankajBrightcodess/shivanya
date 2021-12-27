ab1af518da12d7ac22a967bd69d6d626e3ad5ba1
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-12-27 18:19:41 +0530

    update

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
                 <h2>
                     <a href="centrelogin.php" class="btn btn-warning blink"><strong>Centre Login</strong></a>
                     <a href="centerlist.php" class="btn btn-warning blink"><strong>Centre List</strong></a>
-                    <a href="newcenterenquery.php?page=courses" class="btn btn-warning blink"><strong>Apply For New Centre Enquiry</strong></a>
+                    <a href="newcenterenquery.php?page=courses" class="btn btn-warning blink"><strong>Apply For New Centre Franchise</strong></a>
                     <!-- <a href="results.php" class="btn btn-warning blink"><strong>Results</strong></a> -->
                     
                 </h2>
