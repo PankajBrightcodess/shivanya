95530d01d52601530c5c4c4be1f7207d8610d984
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-05-11 18:02:52 +0530

    update

--- admin/connection.php
+++ admin/connection.php
@@ -3,7 +3,8 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$conn=new mysqli("localhost","root","","db_shivanya");
 }
 else{
-	$conn=new MySQLi("localhost","softwarebss_shivanya","Shivanya@123#","softwarebss_shivanya");
+	// $conn=new MySQLi("localhost","softwarebss_shivanya","Shivanya@123#","softwarebss_shivanya");
+	$conn=new MySQLi("localhost","brightwm_shivanyacomp","Shivanyacomp@123#","brightwm_shivanyacomp");
 }
 if($conn->connect_errno){
 	echo "connection failed";
