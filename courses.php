32d853615901f36737aa0f3bf068f8550a1ce11f
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-11-10 18:07:22 +0530

    Update courses.php

--- courses.php
+++ courses.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Cource | The Shivanya Computer Education</title>
+    <title>Course | The Shivanya Computer Education</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php include"head-links.php" ; ?>
 
