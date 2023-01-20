492f60ed6d1a3f9c413398646afa5a0f64c3bb49
PankajBrightcodess <86092953+PankajBrightcodess@users.noreply.github.com>
2023-01-20 09:49:13 +0530

    Update studymaterial.php

--- student/studymaterial.php
+++ student/studymaterial.php
@@ -65,13 +65,13 @@ while ($data=mysqli_fetch_assoc($run)) {
                            <td>
                             <?php
                             if($course['upload_image']!=''){
-                                    ?><a href="../study_material/image/<?php echo $course['upload_image']; ?>" class="btn btn-sm btn-warning">Image Download</a>
+                                    ?><a href="../study_material/image/<?php echo $course['upload_image']; ?>" Download class="btn btn-sm btn-warning">Image Download</a>
                                     <?php } ?>
                           </td>
                            <td>
                               <?php
                               if($course['upload_pdf']!=''){
-                                    ?><a href="../study_material/pdf/<?php echo $course['upload_pdf']; ?>" class="btn btn-sm btn-success">Pdf Download</a>
+                                    ?><a href="../study_material/pdf/<?php echo $course['upload_pdf']; ?>" Download class="btn btn-sm btn-success">Pdf Download</a>
                                     <?php } ?>
                            </td>
                             <td>
